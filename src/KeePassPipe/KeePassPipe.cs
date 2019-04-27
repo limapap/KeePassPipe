0c2f8765f2eeef64d47c344734771e0cb9b72aa6
limapap@outlook.com <limapap@outlook.com>
2019-04-27 11:19:43 +0200

    Initial release

---
+++ src/KeePassPipe/KeePassPipe.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+
+
+namespace KeePassPipe
+{
+    class KeePassPipe
+    {
+        private static readonly string[] Switches = { "-t", "-u", "-p" };
+        private static string ServerPipeName;
+
+        private static void Main(string[] args) 
+        {
+            const int NumberOfDataLines = 3;
+            string[] Lines=new string[NumberOfDataLines];
+            
+            try
+            {
+                if ((args.Length != 2) || (! Array.Exists(Switches, element => element == args[0])))
+                {
+                    Console.WriteLine("Usage: KeePassPipe [-t|-u|-p] Title");
+                    Environment.Exit(20010);
+                    return;
+                }
+                                          
+                
+                ServerPipeName = "KeePassPipe." + Process.GetCurrentProcess().SessionId;
+                NamedPipeClientStream Connection = new NamedPipeClientStream(
+                    ".", ServerPipeName, PipeDirection.InOut, PipeOptions.None);
+                Connection.Connect(5000);
+
+                StreamReader Reader = new StreamReader(Connection, Encoding.UTF8);
+                StreamWriter Writer = new StreamWriter(Connection, Encoding.UTF8);
+
+                String SearchTitle = args[1];
+                Writer.WriteLine(SearchTitle);
+                Writer.Flush();
+                Connection.Flush();
+
+                for (int i = 0; i < NumberOfDataLines; i++)
+                {
+                    if (Reader.EndOfStream) break;
+                    string response = Reader.ReadLine();
+                    Lines[i] = response;
+                }
+                Connection.Dispose();
+            }
+            catch (Exception e)
+            {
+                int code = 20014;
+                var w32ex = e as System.ComponentModel.Win32Exception;
+                if (w32ex == null)
+                    w32ex = e.InnerException as System.ComponentModel.Win32Exception;
+                if (w32ex != null)
+                    code = w32ex.ErrorCode;
+                Environment.Exit(code);
+                return;
+            }
+
+            int LineNumber = Array.IndexOf(Switches, args[0]);
+            Console.WriteLine(Lines[LineNumber]);
+            Environment.Exit(0);
+        }
+        
+    }
+   
+}
---
+++ src/KeePassPipePlugin/KeePassPipePluginExt.cs
@@ -0,0 +1,133 @@
+﻿using KeePass.Plugins;
+using KeePassLib;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+
+namespace KeePassPipePlugin 
+{
+    public sealed class KeePassPipePluginExt : Plugin
+    {
+        private IPluginHost KeePassHost = null;
+        private string PipeName;
+        private readonly object ThreadLock = new object();
+        private Boolean Terminating = false;
+                       
+        public override bool Initialize(IPluginHost host)
+        {
+            if (host == null) return false; 
+            KeePassHost = host;
+            PipeName = "KeePassPipe." + Process.GetCurrentProcess().SessionId;
+
+            Thread ServerThread = new Thread(ThreadNamedPipeServer);
+            Terminating = false;
+            ServerThread.Start(this);
+            return true;
+        }
+
+        public override void Terminate()
+        {
+            Terminating = true;
+            lock (ThreadLock)
+            {
+                try
+                {
+                    using (NamedPipeClientStream cs = new NamedPipeClientStream(
+                        ".", PipeName, PipeDirection.InOut, PipeOptions.None))
+                    {
+                        cs.Connect(500);
+                    }
+                }
+                catch { }
+            }
+            KeePassHost = null;
+        }
+        
+        private void ThreadNamedPipeServer(object data)
+        {
+            NamedPipeServerStream PipeStream = null;
+            try
+            {
+                while (!Terminating)
+                {
+                    lock (ThreadLock)
+                    {
+                        if (Terminating) return;
+
+                        var sec = new PipeSecurity();
+                        sec.AddAccessRule(
+                            new PipeAccessRule(WindowsIdentity.GetCurrent().Name, PipeAccessRights.FullControl, 
+                            AccessControlType.Allow));
+                        PipeStream = new NamedPipeServerStream(
+                            PipeName, PipeDirection.InOut,1,PipeTransmissionMode.Byte,PipeOptions.None, 0, 0, sec);
+                    }
+
+                    try
+                    {
+                        PipeStream.WaitForConnection();
+                        if (Terminating)
+                        {
+                            PipeStream.Dispose();
+                            return;
+                        }
+                        else
+                        {
+                            StreamReader Reader = new StreamReader(PipeStream, Encoding.UTF8);
+                            StreamWriter Writer = new StreamWriter(PipeStream, Encoding.UTF8);
+
+                            string SearchTitle = Reader.ReadLine();
+                            string[] Lines = GetDataLines(SearchTitle);
+                            foreach (string s in Lines)
+                                Writer.WriteLine(s);
+                            Writer.Flush();
+
+                            PipeStream.Flush();
+                            PipeStream.Close();
+                            PipeStream.Dispose();
+                        }
+                    }
+                    catch { }
+                } 
+            }
+            catch { }
+        }
+
+        private string[] GetDataLines(string ASearchTitle)
+        {
+            const int NumberOfDataLines = 3;
+            string[] Lines = new string[NumberOfDataLines] {"","",""};
+            foreach (var doc in KeePassHost.MainWindow.DocumentManager.Documents)
+            {
+                PwDatabase db = doc.Database;
+                if (db.IsOpen)
+                {
+                    var ItemList = db.RootGroup.GetObjects(true, true);
+                    foreach (var Item in ItemList)
+                    {
+                        if (Item is PwEntry)
+                        {
+                            PwEntry Entry = Item as PwEntry;
+                            string Title = Entry.Strings.ReadSafe("Title");
+                            if (ASearchTitle.Equals(Title))
+                            {
+                                Lines[0] = Title;
+                                Lines[1] = Entry.Strings.ReadSafe("UserName");
+                                Lines[2] = Entry.Strings.ReadSafe("Password");
+                                goto EntryFound;
+                            }
+                        }
+                    }
+                }
+            }
+            EntryFound:
+            return Lines;
+        }
+    }
+}
